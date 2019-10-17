18655007cab71d69d6119566dd15e36b63e61aed
Jan Kondula <jan.kondula@unicorncz.onmicrosoft.com>
2019-10-17 10:11:54 +0200

    fix: pull request

--- api/src/main/resources/db/migration/V2__rename_result_row_table.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE result_row RENAME TO calculation_step;
-CREATE UNIQUE INDEX uc_ship
-    ON ship (name, country);
--- api/src/main/resources/db/migration/V3__add_createdModified.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE calculation
-ADD created_date TIMESTAMP;
-ALTER TABLE calculation
-ADD last_modified_date TIMESTAMP;
