9bfddb03e4436487e01929334437acfeeb11d900
Jostein Solaas <josas@statoil.com>
2020-04-16 08:39:45 +0200

    feat: add database + alembic + pre-commit + init hg

---
+++ main-api/config.py
@@ -0,0 +1,10 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", os.urandom(64))
+    TESTING = False
+    DATABASE_URI = os.environ.get("DATABASE_URI")
+    PORT = os.environ.get("FLASK_RUN_PORT", 8080)
+
+    LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
